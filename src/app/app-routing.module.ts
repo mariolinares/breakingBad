1f88d24c768e2b353ec95f81976dbbeb8147b9f7
Mario <mariolinaresparra@icloud.com>
2021-10-05 12:26:10 +0200

    (create): Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'characters'
+  },
+  {
+    path: 'characters',
+    loadChildren: () => import('./pages/characters/characters.module').then(m => m.CharactersModule)
+  },
+  {
+    path: 'character/:id',
+    loadChildren: () => import('./pages/details/details.module').then(m => m.DetailsModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
