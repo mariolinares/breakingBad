b094624fa4e68f666f8d0d1bb83ca8f0cafdf472
Mario <mariolinaresparra@icloud.com>
2021-10-07 13:36:32 +0200

    (update): Apply translation to header component

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'at-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HeaderComponent{
+  constructor(public translate: TranslateService) { }
 }
