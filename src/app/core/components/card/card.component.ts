34cec85bd8a9183ec5d4094137a8ab66ad2c94e5
Mario <mariolinaresparra@icloud.com>
2021-10-07 02:45:11 +0200

    (update): Improve card component and add changeDetectionStrategy

--- src/app/core/components/card/card.component.ts
+++ src/app/core/components/card/card.component.ts
@@ -1,19 +1,20 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Character } from '../../models/characters.interface';
 
 @Component({
   selector: 'at-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardComponent {
 
   @Input() character: Character;
-  @Output() characterId = new EventEmitter();
+  @Output() characterEmit = new EventEmitter();
 
   doAction(character: Character) {
     console.log('Seleccionado: ', character )
-    this.characterId.emit(character)
+    this.characterEmit.emit(character)
   }
-
+  
 }
