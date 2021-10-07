f25b97c24b885f2bde28306ae1945ddb9e154d77
Mario <mariolinaresparra@icloud.com>
2021-10-07 23:22:14 +0200

    (update): material and shared module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,12 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
@@ -22,9 +19,6 @@ import { MatSelectModule } from '@angular/material/select';
     MatMenuModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatGridListModule,
     MatButtonToggleModule,
     MatSelectModule,
   ],
@@ -37,9 +31,6 @@ import { MatSelectModule } from '@angular/material/select';
     MatMenuModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatGridListModule,
     MatButtonToggleModule,
     MatSelectModule,
   ],
