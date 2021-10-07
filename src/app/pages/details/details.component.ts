545a049dbfdc5497bf835b13757942685b458a68
Mario <mariolinaresparra@icloud.com>
2021-10-07 02:51:25 +0200

    (update): create details section

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Character } from 'src/app/core/models/characters.interface';
-import { Quote } from 'src/app/core/models/quote.interface';
+import { forkJoin, Observable, of } from 'rxjs';
+import { fadeAnimation } from 'src/app/core/animations/animations';
+import { replace } from 'src/app/core/models/regex';
 import { HttpService } from 'src/app/core/services/http.service';
 
 @Component({
   selector: 'at-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
+  animations: [fadeAnimation]
+
 })
 export class DetailsComponent implements OnInit {
   characterName: string;
-  character: Character;
-  quote: Quote;
+  character$: Observable<any>;
+  quote$: Observable<any>;
+  API_URL: string = 'https://www.breakingbadapi.com/api/';
 
+  /**
+   * @param activatedRoute 
+   * @param http 
+   */
   constructor(
     private activatedRoute: ActivatedRoute,
     private http: HttpService
@@ -21,14 +29,20 @@ export class DetailsComponent implements OnInit {
 
   ngOnInit(): void {
     const name: string = this.activatedRoute.snapshot.params.name;
-    this.characterName = name.replace(' ', '+');
+    this.characterName = name.replace(replace, "+")    
     this.getDetails(this.characterName);
   }
 
+  /**
+   * 
+   * @param name String
+   */
   getDetails(name: string) {
-    this.http.getDetails(name).subscribe((data) => {
-      this.character = data.character[0];
-      this.quote = data.quote[0] ? data.quote[0] : '';
-    });
-  }
+    return forkJoin({
+      character: this.http.getData(`${this.API_URL}characters?name=${name}`),
+      quote: this.http.getData(`${this.API_URL}quote/random?author=${name}`),
+    }).subscribe(data => {
+      this.character$ = of(data.character[0]);
+      this.quote$ = of(data.quote[0]);
+    })}
 }
--- src/app/pages/details/details.module.ts
+++ src/app/pages/details/details.module.ts
@@ -4,16 +4,21 @@ import { CommonModule } from '@angular/common';
 import { DetailsRoutingModule } from './details-routing.module';
 import { DetailsComponent } from './details.component';
 import { QuoteComponent } from 'src/app/core/components/quote/quote.component';
+import { MaterialModule } from 'src/app/material.module';
+import { TildePipe } from 'src/app/core/pipes/tilde.pipe';
 
 
 @NgModule({
   declarations: [
     DetailsComponent,
-    QuoteComponent
+    QuoteComponent,
+    TildePipe,
+
   ],
   imports: [
     CommonModule,
-    DetailsRoutingModule
+    DetailsRoutingModule,
+    MaterialModule
   ]
 })
 export class DetailsModule { }
