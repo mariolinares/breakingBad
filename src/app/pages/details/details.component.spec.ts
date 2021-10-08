f6dac7ad24f0a5bd2ea8740c0cb578f3b9029002
Mario <mariolinaresparra@icloud.com>
2021-10-08 14:19:48 +0200

    (update): details component

--- src/app/pages/details/details.component.spec.ts
+++ src/app/pages/details/details.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from 'src/app/shared.module';
 import { DetailsComponent } from './details.component';
@@ -15,6 +16,7 @@ describe('DetailsComponent', () => {
       imports: [
         SharedModule, 
         HttpClientTestingModule,
+        RouterTestingModule,
         TranslateModule.forRoot({
           loader: {
             provide: TranslateLoader,
