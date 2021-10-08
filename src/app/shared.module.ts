4bd5f1c064c4c527d8ce6a3f9454c7e30c5de759
Mario <mariolinaresparra@icloud.com>
2021-10-08 14:22:22 +0200

    (update): add toastModule and ErrorInterceptor

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,19 +1,22 @@
 import { CommonModule, registerLocaleData } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MaterialModule } from './material.module';
+import { ErrorInterceptor } from './core/interceptor/error.interceptor';
+import { ToastrModule } from 'ngx-toastr';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
 }
 
 @NgModule({
-  declarations: [],
+
   imports: [
     CommonModule,
     MaterialModule,
+    ToastrModule.forRoot(),
     TranslateModule.forChild({
       loader: {
         provide: TranslateLoader,
@@ -22,6 +25,13 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
       },
       isolate: false,
     }),
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
   ],
   exports: [TranslateModule, MaterialModule],
 })
